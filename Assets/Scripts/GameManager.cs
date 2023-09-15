1aea7d629dbd9e3d59191335411a44999f8bdcc7
JoaoMoraesJr <joaojr1997@gmail.com>
2023-09-14 22:40:16 -0300

    Organize folders and fix VR scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,12 +10,17 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         player.SetActive(true);
-        player.transform.position = playerSpawn.position + new Vector3(0, 5, 0);
+        Invoke("setPlayerPosition", 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void setPlayerPosition()
+    {
+        player.transform.position = playerSpawn.position + new Vector3(0, 3.5f, 0);
     }
 }
